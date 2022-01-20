574820d1470d948cdc461ad156d7921010fe7e4a
Cihan BAS <cihanbas07@gmail.com>
2022-01-21 02:06:27 +0300

    redux actions created

---
+++ src/redux/store/youtube/actions.ts
@@ -0,0 +1,37 @@
+import {IFetchFailure} from 'types/index';
+import {YoutubeTypes} from './actionTypes';
+import {
+  FetchVideoListRequest,
+  FetchYoutubeListFailure,
+  FetchYoutubeListSuccess,
+  FetchYoutubeListSuccessPayload,
+  IVideoListParam,
+} from './types';
+// ? Fetch Video By Coordinate Actions
+const fetchVideoByCoordRequest = (
+  payload: IVideoListParam,
+): FetchVideoListRequest => ({
+  type: YoutubeTypes.GET_LIST_REQUEST,
+  payload,
+});
+
+const fetchVideoByCoordSuccess = (
+  payload: FetchYoutubeListSuccessPayload,
+): FetchYoutubeListSuccess => ({
+  type: YoutubeTypes.GET_LIST_SUCCESS,
+  payload,
+});
+
+const fetchVideoByCoordFailure = (
+  payload: IFetchFailure,
+): FetchYoutubeListFailure => ({
+  type: YoutubeTypes.GET_LIST_FAILURE,
+  payload,
+});
+// ? Fetch More Video Actions
+
+export {
+  fetchVideoByCoordRequest,
+  fetchVideoByCoordSuccess,
+  fetchVideoByCoordFailure,
+};
