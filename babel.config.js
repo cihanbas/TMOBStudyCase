4dba958bfd535ebded6cc5817528340eb4fa262b
Cihan BAS <cihanbas07@gmail.com>
2022-01-20 13:09:05 +0300

    absolute import configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ['./src'],
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+
+        alias: {
+          components: './src/components',
+          screens: './src/screens',
+          app: './src/assets',
+        },
+      },
+    ],
+  ],
 };
