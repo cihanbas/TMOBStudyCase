9e352f5b2f786143254f81855faa920ab0f33c99
Cihan BAS <cihanbas07@gmail.com>
2022-01-21 02:10:10 +0300

    redux added  to the  root page

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import Navigation from 'navigation';
 import React from 'react';
+import {Provider} from 'react-redux';
+import store from 'redux/store';
+import 'react-native-gesture-handler';
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
+  );
 }
