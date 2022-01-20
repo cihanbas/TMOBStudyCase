1b1fd2ce96d51434969375aee1737656bfb2d186
Cihan BAS <cihanbas07@gmail.com>
2022-01-21 02:11:23 +0300

    new color, listheight, isIos added to constatns

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,14 +1,20 @@
+import {Platform} from 'react-native';
 import {moderateScale} from 'react-native-size-matters';
 
 const colors = {
   primary: '#007acc',
   secondary: 'red',
+  white: 'white',
+  black: 'black',
 };
 export const spacing = [0, 4, 8, 12, 16, 24, 32, 48, 64];
+const youtubeAPIKEY = 'AIzaSyBYx06ljYCbidxklTUsj3irXhCfSta0WBM';
 
 function normalize(number: number, factor = 0.25) {
   return moderateScale(number, factor);
 }
-const appPadding = normalize(spacing[5]);
+const isIOS = Platform.OS === 'ios';
 
-export {colors, normalize, appPadding};
+const appPadding = normalize(spacing[5]);
+const VideoItemHeight = 100;
+export {colors, normalize, appPadding, youtubeAPIKEY, isIOS, VideoItemHeight};
