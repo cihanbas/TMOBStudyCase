86b41bf16feb01b86ea4a3b22bfed56d0162d09a
Cihan BAS <cihanbas07@gmail.com>
2022-01-21 10:01:04 +0300

    gesture handler removed (unused)

--- App.tsx
+++ App.tsx
@@ -2,7 +2,6 @@ import Navigation from 'navigation';
 import React from 'react';
 import {Provider} from 'react-redux';
 import store from 'redux/store';
-import 'react-native-gesture-handler';
 
 export default function App() {
   return (
